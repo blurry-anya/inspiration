d445e67961faade98b9df1e96eea407a7ae346af
AnyaKononchenko <anya.kononchenko@gmail.com>
2023-02-06 19:42:45 +0100

    added welcome page animation

--- scripts/welcomePage.js
+++ scripts/welcomePage.js
@@ -1,87 +1,107 @@
 $(window).on('load', function () {
     "use strict";
 
-    // code for pasting js into css starts
-    // credit: https://stackoverflow.com/questions/59573722/how-can-i-set-a-css-keyframes-in-javascript
-    let dynamicStyles = null;
-
-    function addAnimation(body) {
-        if (!dynamicStyles) {
-            dynamicStyles = document.createElement('style');
-            dynamicStyles.type = 'text/css';
-            document.head.appendChild(dynamicStyles);
-        }
-
-        dynamicStyles.sheet.insertRule(body, dynamicStyles.length);
-    }
-    // code for pasting js into css ends
-
     const ballsSection = document.querySelector("#balls-animation");
-    console.log(ballsSection.offsetWidth);
 
-    const startPositionX = ballsSection.offsetWidth/2;
-    const startPositionY = ballsSection.offsetHeight/2;
+    const sectionWidth = ballsSection.offsetWidth;
+    const sectionHeight = ballsSection.offsetHeight;
 
-    const borderX = ballsSection.offsetWidth - 20;
-    const borderY = ballsSection.offsetHeight - 20;
+    const startPositionX = sectionWidth / 2;
+    const startPositionY = sectionHeight / 2;
+
+    function Ball(x = 0, y = 0, width = 10, height = 10, directionX = 1, directionY = 1, speed = 1, id = 0, move = false) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.speed = speed;
+        this.id = id;
+        this.isMove = false;
+    }
+
+    const ballWidth = 30;
+    const ballHeight = 30;
+    const ballSpeed = 3;
+
+    const borderX = sectionWidth - ballWidth - 10;
+    const borderY = sectionHeight - ballHeight;
 
     console.log(startPositionX, startPositionY)
     console.log(borderX, borderY)
-    
 
-    addAnimation(`
-        @keyframes ball-moves-x {
-            from{
-                right: ${startPositionX}px;
-            } to {
-                right: ${borderX}px;
-            }
+    let getRandomDirection = () => {
+        let res = getRandomInt(2);
+        return res <= 0 ? res -= 1 : res;
+    }
+
+    let balls = [], ballsObjects = [];
+    for (let i = 0, index = 1; i < 20; i++) {
+        balls.push(document.createElement("div"));
+        ballsObjects.push(new Ball(startPositionX + index, startPositionY + index, ballWidth, ballHeight,
+            getRandomDirection() * (index / 3), getRandomDirection() * (index / 3), ballSpeed, i, false));
+        index += 1;
+    }
+
+    balls.map(ball => ball.className = "ball");
+    balls.map(ball => ball.style.backgroundColor = getRandomColor());
+    balls.map(ball => ball.style.width = `${ballWidth}px`);
+    balls.map(ball => ball.style.height = `${ballWidth}px`);
+    balls.map(ball => ball.style.left = `${startPositionX}`);
+    balls.map(ball => ball.style.top = `${startPositionY}`);
+    balls.map(ball => ballsSection.appendChild(ball));
+
+    let stopFlag = true;
+    document.querySelector(".start-btn").addEventListener("click", function () {
+        stopFlag = !stopFlag ? true : false;
+        loop();
+
+        if(!stopFlag){
+            this.innerText = "Impressive, but can you stop the time?";
+        } else {
+            this.innerText = "Wow, that was so cool!";
+            setTimeout(()=>{
+                this.innerText = "Let's continue? :)";
+            }, 2000)
         }
-    `);
-
-    addAnimation(`
-        @keyframes ball-moves-y {
-            from{
-                top: ${startPositionY}px;
-            } to {
-                top: ${startPositionY}px;
-            }
-        }
-    `);
-    
-    
-
-    let ball = document.createElement("div");
-    ball.className = "ball";
-    ball.style.backgroundColor = getColor();
-    
-    console.log(ball);
-
-    
-
-    document.querySelector(".start-btn").addEventListener("click",function(){
-        ballsSection.appendChild(ball);
-        ball.style.animation = "2s linear 0s infinite alternate ball-moves-x, 2s linear 0s infinite alternate ball-moves-y";
     });
 
-    // let main = document.querySelector("main");
+    function loop() {
+        if(!stopFlag){
+            window.requestAnimationFrame(loop);
+            // console.log("running");
+            ballsObjects.forEach(ballObject => {
+                if (ballObject.x > borderX || ballObject.x < 0) { // bounce at right and left
+                    ballObject.directionX *= -1;
+                }
+                if (ballObject.y > borderY || ballObject.y < 0) { // bounce at bottom and top
+                    ballObject.directionY *= -1;
+                }
+                ballObject.x += ballObject.directionX * ballObject.speed;
+                ballObject.y += ballObject.directionY * ballObject.speed;
+    
+                balls[ballObject.id].style.left = `${ballObject.x}px`;
+                balls[ballObject.id].style.top = `${ballObject.y}px`;
+    
+                ballObject.isMove = true;
+            });
+        } else {
+            window.cancelAnimationFrame(loop);
+        }
+    }
 
-    // let balls = [];
-    // for (let i = 0; i < 5; i++) {
-    //     balls.push(document.createElement("div"));
-    // }
+    balls.forEach(ball => {
+        ball.addEventListener('click', () =>{
+            ball.classList.add('add-smile');
+            setTimeout(() => {
+                ball.classList.remove('add-smile');
+            }, 2000);
+        })
+    });
 
-    // balls.map(ball => ball.className = "ball");
-    // balls.map(ball => ball.style.backgroundColor = getColor());
-    // balls.map(ball => ball.style.animation = "4s linear 0s infinite alternate move-eye");
-    // balls.map(ball => main.appendChild(ball));
-
-
-    // console.log(balls)
-
-
-    function getColor() {
-        let colors = ["red", "blue", "yellow", "green", "orange", "pink", "purple", "white"];
+    function getRandomColor() {
+        let colors = ["#FAFFD8", "#ECFFB0", "#9AA899", "#54577C", "#4A7B9D", "#482C3D", "#AC3931", "#596157", "#CFD186", "#5B8C5A"];
         return colors[getRandomInt(colors.length)]
     }
 
